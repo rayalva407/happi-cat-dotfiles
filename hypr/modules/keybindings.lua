6d828d93a5f59781913f1cf709d2542ab939971b
Ray Alva <rayalva407@gmail.com>
2026-09-06 21:01:44 -0500

    refactor: changes the keybinding for hyprlauncher to mod space

--- hypr/modules/keybindings.lua
+++ hypr/modules/keybindings.lua
@@ -8,14 +8,14 @@ local shiftMod = "SHIFT + SUPER"
 local ctrlMod = "CTRL + SUPER"
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + return", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 -- hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + space", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
-hl.bind(mainMod .. " + SEMICOLON", hl.dsp.layout("togglesplit"))    -- dwindle only
+hl.bind(mainMod .. " + semicolon", hl.dsp.layout("togglesplit"))    -- dwindle only
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen())
 
 -- Navigation & Focus Management
